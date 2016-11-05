dc9c84b8eff23efa05f27789f6583fe9304391a9
Pedro Rosanes <prosanes@gmail.com>
2016-11-05 21:15:31 -0200

    First ex7 solution

--- machine-learning-ex7/ex7/findClosestCentroids.m
+++ machine-learning-ex7/ex7/findClosestCentroids.m
@@ -21,7 +21,20 @@ idx = zeros(size(X,1), 1);
 % Note: You can use a for-loop over the examples to compute this.
 %
 
+for i = 1:size(X,1)
+  lower_dist = Inf;
+  lower_idx = 1;
+  
+  for j = 1:K
+    dist = sum((X(i,:) - centroids(j,:)) .^ 2);
+    if dist < lower_dist
+      lower_dist = dist;
+      lower_idx = j;
+    endif
+  endfor
 
+  idx(i) = lower_idx;
+endfor
 
 
 
