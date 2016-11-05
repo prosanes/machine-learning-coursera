442e6f12187d03e14894fb1c8b2bdeabd06cc99d
Pedro Rosanes <prosanes@gmail.com>
2016-11-05 21:22:27 -0200

    Second ex in ex7

--- machine-learning-ex7/ex7/computeCentroids.m
+++ machine-learning-ex7/ex7/computeCentroids.m
@@ -26,6 +26,10 @@ centroids = zeros(K, n);
 % Note: You can use a for-loop over the centroids to compute this.
 %
 
+for i = 1:K
+  xk = X(idx==i, :);
+  centroids(i,:) = sum(xk) / size(xk, 1);
+endfor
 
 
 
