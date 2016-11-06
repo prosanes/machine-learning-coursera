37e73b5e3a81b76460a5ad7fa4301e57f5249d34
Pedro Rosanes <prosanes@gmail.com>
2016-11-05 22:50:24 -0200

    Second ex of ex8

--- machine-learning-ex8/ex8/selectThreshold.m
+++ machine-learning-ex8/ex8/selectThreshold.m
@@ -23,17 +23,31 @@ for epsilon = min(pval):stepsize:max(pval)
     % Note: You can use predictions = (pval < epsilon) to get a binary vector
     %       of 0's and 1's of the outlier predictions
 
+    pred = pval < epsilon;
 
+    tp = 0;
+    fp = 0;
+    fn = 0;
+    tn = 0;
+    for i = 1:size(pred)
+      if yval(i) == 1
+        if pred(i) == yval(i)
+          tp += 1;
+        else
+          fp += 1;
+        end
+      else
+        if pred(i) == yval(i)
+          tn += 1;
+        else
+          fn += 1;
+        end
+      end
+    end
 
-
-
-
-
-
-
-
-
-
+    prec = tp / (tp + fp);
+    rec = tp / (tp + fn);
+    F1 = (2*prec*rec)/(prec+rec);
 
     % =============================================================
 
